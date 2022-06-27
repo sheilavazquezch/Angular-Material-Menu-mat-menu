ba3d2d4477ab7e00bcbb317e9e78d72d91eb0b80
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-27 00:34:31 -0500

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JuegodadosComponent } from './juegodados/juegodados.component';
+import { DadoComponent } from './dado/dado.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegodadosComponent,
+    DadoComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
